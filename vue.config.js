a34c0acad17e639e310226af014cca8bc94f2a89
Yin Zihang <201250173@smail.nju.edu.cn>
2023-12-31 13:25:03 +0800

    解决打包好页面空白的问题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: './',
   configureWebpack: {
     devServer: {
       port: 8000,
@@ -6,7 +7,7 @@ module.exports = {
       host:"localhost",
       proxy: {
         '/api': {
-          target: 'http://localhost:8888',
+          target: 'http://1.94.21.181:8888',
           ws: true,
           changeOrigin: true,
           pathRewrite:{
